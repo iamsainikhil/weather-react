17f0b763b92163727f2bacd55d801f0e9f2051f2
iamsainikhil <sainikhil.bheema@gmail.com>
2020-04-13 20:13:33 -0400

    v0.7.2 - fix root directory URL for assets on prod environment

--- src/utils/AssetsSrcURL.js
+++ src/utils/AssetsSrcURL.js
@@ -1,2 +1,3 @@
-// this URL points to the root directory of the app
-export default './weather-react'
+// this URL points to the root directory of the app based on dev or prod environment
+const isProduction = process.env.NODE_ENV === 'production'
+export default isProduction ? '.' : './weather-react'
