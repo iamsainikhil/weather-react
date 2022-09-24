6c6ab5376ceed57e0ca9632fd806202350bfad45
iamsainikhil <sainikhil.bheema@gmail.com>
2022-09-24 00:35:20 -0400

    fix(forecast): update temperature units

--- api/forecast.js
+++ api/forecast.js
@@ -5,7 +5,7 @@ const API_KEY = process.env.OPENWEATHER_API_KEY
 export default function handler(req, res) {
   const {latlong} = req.query
   const [lat, long] = latlong ? latlong.split(',') : [0, 0]
-  const url = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${long}&exclude=minutely&appid=${API_KEY}`
+  const url = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${long}&exclude=minutely&units=imperial&appid=${API_KEY}`
   axios
     .get(url)
     .then((response) => {
