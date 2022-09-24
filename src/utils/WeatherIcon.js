be60a436809fbfa130f4d0fbb4118f7b9e0a2ded
iamsainikhil <sainikhil.bheema@gmail.com>
2022-09-23 22:25:03 -0400

    refactor: update weather icons and background

--- src/utils/WeatherIcon.js
+++ src/utils/WeatherIcon.js
@@ -1,45 +1,37 @@
 import FormatTime from './FormatTime'
+import isWithinRange from './IsWithinRange'
 
 /**
  * @param {Object} data (weatherCurrent, Timeframe, day)
  */
 const getWeatherIcon = (data) => {
-  const {icon, time, timezone} = data
-  const hour = FormatTime(time, timezone, 'H')
+  const {weather, dt, timezone} = data
+  const {id: iconId} = weather[0]
+  const hour = FormatTime(dt, timezone, 'H')
   const type = hour >= 6 && hour <= 18 ? 'day' : 'night'
-  if (icon) {
-    switch (icon) {
-      case 'clear-day':
-        return 'day'
-      case 'clear-night':
-        return 'night'
-      case 'rain':
-        return `${type}-rain`
-      case 'snow':
-        return `${type}-snow`
-      case 'sleet':
-        return 'sleet'
-      case 'wind':
-        return `${type}-cloudy`
-      case 'fog':
-        return `${type}-cloudy`
-      case 'cloudy':
-        return 'cloudy'
-      case 'partly-cloudy-day':
-        return 'day-cloudy'
-      case 'partly-cloudy-night':
-        return 'night-cloudy'
-      case 'hail':
-        return 'hail'
-      case 'thunderstorm':
-        return 'thunder'
-      case 'tornado':
-        return 'wi-tornado'
-      default:
-        return 'wi-na'
-    }
+
+  if (iconId === 800) {
+    return `${type}`
+  } else if (iconId === 781) {
+    return 'wi-tornado'
+  } else if (isWithinRange(200, 232, iconId)) {
+    return 'thunder'
+  } else if (isWithinRange(300, 321, iconId)) {
+    return 'hail'
+  } else if (isWithinRange(500, 531, iconId)) {
+    return `${type}-rain`
+  } else if (isWithinRange(611, 613, iconId)) {
+    return 'sleet'
+  } else if (isWithinRange(600, 622, iconId)) {
+    return `${type}-snow`
+  } else if (
+    isWithinRange(701, 781, iconId) ||
+    isWithinRange(801, 804, iconId)
+  ) {
+    return `${type}-cloudy`
+  } else {
+    return 'wi-na'
   }
-  return 'wi-na'
 }
 
 export default getWeatherIcon
